1c537b10732ece3dfe26efc4b17b4a25e3f42aed
Thoq <125930852+Thoq-jar@users.noreply.github.com>
2024-06-17 18:53:14 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 /* JS GOES HERE */
+
+const text = document.getElementById('text');
+const button = document.getElementById('btn1')
+let buttoncounter = 0;
+
+button.addEventListener('click', function() {
+    buttoncounter++;
+    text.textContent = `Button clicked ${buttoncounter} times!`;
+}); 
